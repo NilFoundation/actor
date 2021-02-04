98e94024c53925d57682e57720d51aa670a0505b
Mikhail Komarov <nemo@nil.foundation>
2021-02-04 09:49:29 +0300

    Submodules updated.

--- docker/compiler-explorer/start-ce.sh
+++
@@ -1,4 +0,0 @@
-git -C /seastar-1808 pull
-git -C /seastar-master pull
-git -C /ultramarine pull
-make -C /compiler-explorer run
--- libs/cluster/README.md
+++
@@ -1,4 +0,0 @@
-CUDA Actors
-=============
-
-This module eases the use of CUDA with =nil; Actor.
