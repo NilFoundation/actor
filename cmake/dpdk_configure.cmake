01736dc2a646582daee50a0ad34f69db7f5bd742
Mikhail Komarov <nemo@nil.foundation>
2021-04-24 15:51:40 +0300

    Submodules updated.

--- cmake/dpdk_configure.cmake
+++ cmake/dpdk_configure.cmake
@@ -20,8 +20,8 @@
 # Copyright (C) 2018 Scylladb, Ltd.
 #
 
-file (READ ${Seastar_DPDK_CONFIG_FILE_IN} dpdk_config)
-file (STRINGS ${Seastar_DPDK_CONFIG_FILE_CHANGES} dpdk_config_changes)
+file (READ ${ACTOR_DPDK_CONFIG_FILE_IN} dpdk_config)
+file (STRINGS ${ACTOR_DPDK_CONFIG_FILE_CHANGES} dpdk_config_changes)
 set (word_pattern "[^\n\r \t]+")
 
 foreach (var ${dpdk_config_changes})
@@ -37,5 +37,5 @@ foreach (var ${dpdk_config_changes})
   endif ()
 endforeach ()
 
-file (WRITE ${Seastar_DPDK_CONFIG_FILE_OUT} ${dpdk_config})
-file (APPEND ${Seastar_DPDK_CONFIG_FILE_OUT} "CONFIG_RTE_MACHINE=${Seastar_DPDK_MACHINE}")
+file (WRITE ${ACTOR_DPDK_CONFIG_FILE_OUT} ${dpdk_config})
+file (APPEND ${ACTOR_DPDK_CONFIG_FILE_OUT} "CONFIG_RTE_MACHINE=${ACTOR_DPDK_MACHINE}")
